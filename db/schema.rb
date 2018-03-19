342370c71028eec1d42825b51aa2eae21b188481
Alexander Kiessling <ajkiessl@gmail.com>
2018-03-19 15:43:05 -0400

    Revamped database to have one table 'contract_grants'.  Finished osp_data.rake to populate database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180316184731) do
+ActiveRecord::Schema.define(version: 20180319184109) do
 
-  create_table "contracts", force: :cascade do |t|
-    t.string "title"
-    t.integer "pub_fac_link_id"
-    t.integer "sponsor_id"
-    t.string "status"
+  create_table "contract_grants", force: :cascade do |t|
     t.integer "osp_key"
+    t.string "title"
+    t.string "sponsor_name"
+    t.string "sponsor_type"
+    t.string "access_id"
+    t.string "f_name"
+    t.string "l_name"
+    t.string "role"
+    t.integer "pct_credit"
+    t.string "status"
     t.date "submitted"
     t.date "awarded"
     t.integer "requested"
@@ -27,36 +32,6 @@ ActiveRecord::Schema.define(version: 20180316184731) do
     t.date "end_date"
     t.string "grant_contract"
     t.string "base_agreement"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["pub_fac_link_id"], name: "index_contracts_on_pub_fac_link_id"
-    t.index ["sponsor_id"], name: "index_contracts_on_sponsor_id"
-  end
-
-  create_table "faculties", force: :cascade do |t|
-    t.string "f_name"
-    t.string "l_name"
-    t.string "username"
-    t.integer "pub_fac_link_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["pub_fac_link_id"], name: "index_faculties_on_pub_fac_link_id"
-  end
-
-  create_table "pub_fac_links", force: :cascade do |t|
-    t.integer "contract_id"
-    t.integer "faculty_id"
-    t.string "role"
-    t.integer "pct_credit"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["contract_id"], name: "index_pub_fac_links_on_contract_id"
-    t.index ["faculty_id"], name: "index_pub_fac_links_on_faculty_id"
-  end
-
-  create_table "sponsors", force: :cascade do |t|
-    t.string "sponsor_name"
-    t.string "sponsor_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
