c421fec8c2fa89a67678a0a2a7ce6e7dc8017201
Alexander Kiessling <ajkiessl@gmail.com>
2018-04-09 15:17:13 -0400

    Building tests and refactoring.

--- lib/osp_format.rb
+++ lib/osp_format.rb
@@ -1,8 +1,9 @@
 require 'spreadsheet'
 require 'csv'
+require 'byebug'
 
 class OspFormat
-  attr_reader :csv_object, :xls_object, :csv_hash
+  attr_accessor :csv_object, :xls_object, :csv_hash
   #Creates CSV and XLS object.  Imported CSV must be tab delimited text.
   def initialize(csv_array = CSV.read('data/dmresults-tabdel.txt', encoding: "ISO8859-1", col_sep: "\t"), 
                  xls_object = Spreadsheet.open('data/psu-users.xls'))
@@ -12,9 +13,14 @@ class OspFormat
     @active_users = find_active_users
   end
 
-  def run
+  def format
     format_grant_contract
     format_accessid_field
+    format_role_field
+    format_date_fields
+    format_pending
+    format_start_end
+    remove_columns
   end
 
   def convert_to_hash(csv_array)
@@ -22,107 +28,41 @@ class OspFormat
     csv_array[1..-1].map {|a| Hash[ keys.zip(a) ] }
   end
 
-  #Converts 'Co-PI' to 'Co-Principal Investigator'
-  def format_role_field
-    csv_hash.each do |csv|
-      if csv['role'] == 'Co-PI'
-        csv['role'] = 'Co-Principal Investigator'
-      end
-      if csv['role'] == 'Faculty'
-        csv['role'] = 'Core Faculty'
-      end
-      if csv['role'] == 'Post Doctoral'
-        csv['role'] = 'Post Doctoral Associate'
-      end
-      if csv['role'] == 'unknown'
-        csv['role'] = 'Unknown'
-      end
-    end
-  end
-
-  #Removes time, '/', and '/ /' from date fields.  Convert mm/dd/yy to Date object 
-  def format_date_fields
-    index_arr = [11, 12, 16, 17]
-    csv_object.each do |csv|
-      index_arr.each do |i|
-        if csv[i] == '/' || csv[i] == '/  /'
-          csv[i] = ''
-        end
-        begin
-          date = Date.strptime(csv[i], "%m/%d/%Y")
-          csv[i] = date.strftime("%Y-%m-%d")
-        rescue ArgumentError => e
-          #puts e
-        end
-      end
-    end
-  end
-
-  #Changes 'Pending Award' and 'Pending Proposal' status to 'Pending'
-  def format_pending
-    csv_object.each do |csv|
-      if csv[10] == 'Pending Proposal' || csv[10] == 'Pending Award'
-        csv[10] = 'Pending'
-      end
-    end
-  end
-
-  #Removes start and end dates for any contract that was not 'Awarded'
-  def format_start_end
-    csv_object.each do |csv|
-      unless csv[10] == 'Awarded'
-        csv[16] = ''
-        csv[17] = ''
-      end
-    end
-  end
-
   #Remove rows with 'submitted' dates <= 2011
   def filter_by_date
     kept_rows = []
-    csv_object.each do |csv|
-      if (csv[11].split('-')[0].to_i >= 2011) && (csv[11].split('-')[0].to_i <= 2018) 
+    csv_hash.each do |csv|
+      if (csv['submitted'].split('-')[0].to_i >= 2011) && (csv['submitted'].split('-')[0].to_i <= 2018) 
         kept_rows << csv
       end
     end
-    @csv_object = kept_rows
-  end
-
-  #Remove columns we don't need
-  def remove_columns
-    index_arr = [1, 5, 7, 18, 19, 22, 23]
-    csv_object.each do |csv|
-      index_arr.each do |i|
-        csv[i] = nil
-      end
-      csv.compact!
-    end
+    @csv_hash = kept_rows
   end
 
   #Remove rows that contain non-active users
   def filter_by_user
     kept_rows = []
-    csv_object.each do |csv|
+    csv_hash.each do |csv|
       @active_users.each do |user|
-        if user[3] == csv[4]
-          csv = csv.insert(5, user[2])
-          csv = csv.insert(5, user[0])
-          csv = csv.insert(5, user[1])
+        if user[3] == csv['accessid']
+          csv['m_name'] = user[2]
+          csv['l_name'] = user[0]
+          csv['f_name'] = user[1]
           kept_rows << csv
         end
       end
     end
-    @csv_object = kept_rows
+    @csv_hash = kept_rows
   end
 
   #Remove rows with 'Purged' or 'Withdrawn' status
   def filter_purged_withdrawn
     kept_rows = []
-    csv_object.each do |csv|
-      unless (csv[10] == 'Purged') || (csv[10] == 'Withdrawn')
+    csv_hash.each do |csv|
+      unless (csv['status'] == 'Purged') || (csv['status'] == 'Withdrawn')
         kept_rows << csv
       end
-      @csv_object = kept_rows
+      @csv_hash = kept_rows
     end
   end
 
@@ -179,6 +119,74 @@ class OspFormat
           csv['accessid'] = csv['accessid'].split('-').join("").downcase
         end
       end
+    end
+  end
+
+  #Converts 'Co-PI' to 'Co-Principal Investigator', 'Faculty' to 'Core Faculty', 
+  #'Post Doctoral' to 'Post Doctoral Associate', and 'unknown' to 'Unknown'
+  def format_role_field
+    csv_hash.each do |csv|
+      if csv['role'] == 'Co-PI'
+        csv['role'] = 'Co-Principal Investigator'
+      end
+      if csv['role'] == 'Faculty'
+        csv['role'] = 'Core Faculty'
+      end
+      if csv['role'] == 'Post Doctoral'
+        csv['role'] = 'Post Doctoral Associate'
+      end
+      if csv['role'] == 'unknown'
+        csv['role'] = 'Unknown'
+      end
+    end
+  end
+
+  #Removes time, '/', and '/ /' from date fields.  Convert mm/dd/yy to Date object 
+  def format_date_fields
+    key_arr = ['submitted', 'awarded', 'startdate', 'enddate']
+    csv_hash.each do |csv|
+      key_arr.each do |k|
+        if csv[k] == '/' || csv[k] == '/  /'
+          csv[k] = ''
+        end
+        begin
+          date = Date.strptime(csv[k], "%m/%d/%Y")
+          csv[k] = date.strftime("%Y-%m-%d")
+        rescue ArgumentError => e
+          #puts e
+        end
+      end
+    end
+  end
+
+  #Changes 'Pending Award' and 'Pending Proposal' status to 'Pending'
+  def format_pending
+    csv_hash.each do |csv|
+      if csv['status'] == 'Pending Proposal' || csv['status'] == 'Pending Award'
+        csv['status'] = 'Pending'
+      end
+    end
+  end
+
+  #Removes start and end dates for any contract that was not 'Awarded'
+  def format_start_end
+    csv_hash.each do |csv|
+      unless csv['status'] == 'Awarded'
+        csv['startdate'] = ''
+        csv['enddate'] = ''
+      end
+    end
+  end
+
+  #Remove columns we don't need
+  def remove_columns
+    index_arr = ['ordernumber', 'parentsponsor', 'department', 'totalstartdate', 
+                 'totalenddate', 'agreementtype', 'agreement']
+    csv_hash.each do |csv|
+      index_arr.each do |i|
+        csv[i] = nil
+      end
+      csv.compact!
     end
   end
 
