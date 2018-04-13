5912c492c67756d71ae75208e1ae65ce817fa7d9
Alexander Kiessling <ajkiessl@gmail.com>
2018-04-13 15:09:52 -0400

    Still working to remove duplicates.

--- lib/osp_xml_builder.rb
+++ lib/osp_xml_builder.rb
@@ -7,73 +7,75 @@ class OspXMLBuilder
 
   #Chunks osp data into batches so we don't overload AI with records
   def batched_osp_xml
-    faculty_batch = []
-    @faculties.each do |faculty|
-      faculty_batch << build_xml(faculty)
+    xml_batches = []
+    @faculties.each_slice(250) do |batch|
+      xml_batches << build_xml(batch)
     end
-    return faculty_batch 
+    return xml_batches 
   end
 
   private
-  #Generates xml from faculties table
-  def build_xml(faculty)
+  #Generates xml from a batch of faculty objects
+  def build_xml(batch)
     builder = Nokogiri::XML::Builder.new do |xml|
       xml.Data {
-        xml.Record( 'username' => faculty.access_id ) {
-          faculty.contract_faculty_links.each do |link|
-            xml.CONGRANT {
-              xml.OSPKEY_ link.contract.osp_key
-              xml.BASE_AGREE_ link.contract.base_agreement
-              xml.TYPE_ link.contract.grant_contract
-              xml.TITLE_ link.contract.title
-              xml.SPONORG_ link.contract.sponsor.sponsor_name
-              xml.AWARDORG_ link.contract.sponsor.sponsor_type
-              xml.CONGRANT_INVEST {
-                xml.FACULTY_NAME_
-                xml.FNAME_ link.faculty.f_name
-                xml.MNAME_ link.faculty.m_name
-                xml.LNAME_ link.faculty.l_name
-                xml.ROLE_ link.role
-                xml.ASSIGN_ link.pct_credit
+        batch.each do |faculty|
+          xml.Record( 'username' => faculty.access_id ) {
+            faculty.contract_faculty_links.each do |link|
+              xml.CONGRANT {
+                xml.OSPKEY_ link.contract.osp_key
+                xml.BASE_AGREE_ link.contract.base_agreement
+                xml.TYPE_ link.contract.grant_contract
+                xml.TITLE_ link.contract.title
+                xml.SPONORG_ link.contract.sponsor.sponsor_name
+                xml.AWARDORG_ link.contract.sponsor.sponsor_type
+                xml.CONGRANT_INVEST {
+                  xml.FACULTY_NAME_
+                  xml.FNAME_ link.faculty.f_name
+                  xml.MNAME_ link.faculty.m_name
+                  xml.LNAME_ link.faculty.l_name
+                  xml.ROLE_ link.role
+                  xml.ASSIGN_ link.pct_credit
+                }
+                xml.AMOUNT_REQUEST_ link.contract.requested
+                xml.AMOUNT_ANTICIPATE_ link.contract.total_anticipated
+                xml.AMOUNT_ link.contract.funded
+                xml.STATUS_ link.contract.status
+                xml.DTM_SUB_ Date.strptime(link.contract.submitted.to_s, '%Y-%m-%d').strftime('%B')
+                xml.DTD_SUB_ Date.strptime(link.contract.submitted.to_s, '%Y-%m-%d').strftime('%d')
+                xml.DTY_SUB_ Date.strptime(link.contract.submitted.to_s, '%Y-%m-%d').strftime('%Y')
+                begin
+                  xml.DTM_AWARD_ Date.strptime(link.contract.awarded.to_s, '%Y-%m-%d').strftime('%B')
+                  xml.DTD_AWARD_ Date.strptime(link.contract.awarded.to_s, '%Y-%m-%d').strftime('%d')
+                  xml.DTY_AWARD_ Date.strptime(link.contract.awarded.to_s, '%Y-%m-%d').strftime('%Y')
+                rescue ArgumentError
+                  xml.DTM_AWARD_
+                  xml.DTD_AWARD_
+                  xml.DTY_AWARD_
+                end
+                begin
+                  xml.DTM_START_ Date.strptime(link.contract.start_date.to_s, '%Y-%m-%d').strftime('%B')
+                  xml.DTD_START_ Date.strptime(link.contract.start_date.to_s, '%Y-%m-%d').strftime('%d')
+                  xml.DTY_START_ Date.strptime(link.contract.start_date.to_s, '%Y-%m-%d').strftime('%Y')
+                rescue ArgumentError
+                  xml.DTM_START_
+                  xml.DTD_START_
+                  xml.DTY_START_
+                end
+                begin
+                  xml.DTM_END_ Date.strptime(link.contract.end_date.to_s, '%Y-%m-%d').strftime('%B')
+                  xml.DTD_END_ Date.strptime(link.contract.end_date.to_s, '%Y-%m-%d').strftime('%d')
+                  xml.DTY_END_ Date.strptime(link.contract.end_date.to_s, '%Y-%m-%d').strftime('%Y')
+                rescue ArgumentError
+                  xml.DTM_END_
+                  xml.DTD_END_
+                  xml.DTY_END_
+                end
               }
-              xml.AMOUNT_REQUEST_ link.contract.requested
-              xml.AMOUNT_ANTICIPATE_ link.contract.total_anticipated
-              xml.AMOUNT_ link.contract.funded
-              xml.STATUS_ link.contract.status
-              xml.DTM_SUB_ Date.strptime(link.contract.submitted.to_s, '%Y-%m-%d').strftime('%B')
-              xml.DTD_SUB_ Date.strptime(link.contract.submitted.to_s, '%Y-%m-%d').strftime('%d')
-              xml.DTY_SUB_ Date.strptime(link.contract.submitted.to_s, '%Y-%m-%d').strftime('%Y')
-              begin
-                xml.DTM_AWARD_ Date.strptime(link.contract.awarded.to_s, '%Y-%m-%d').strftime('%B')
-                xml.DTD_AWARD_ Date.strptime(link.contract.awarded.to_s, '%Y-%m-%d').strftime('%d')
-                xml.DTY_AWARD_ Date.strptime(link.contract.awarded.to_s, '%Y-%m-%d').strftime('%Y')
-              rescue ArgumentError
-                xml.DTM_AWARD_
-                xml.DTD_AWARD_
-                xml.DTY_AWARD_
-              end
-              begin
-                xml.DTM_START_ Date.strptime(link.contract.start_date.to_s, '%Y-%m-%d').strftime('%B')
-                xml.DTD_START_ Date.strptime(link.contract.start_date.to_s, '%Y-%m-%d').strftime('%d')
-                xml.DTY_START_ Date.strptime(link.contract.start_date.to_s, '%Y-%m-%d').strftime('%Y')
-              rescue ArgumentError
-                xml.DTM_START_
-                xml.DTD_START_
-                xml.DTY_START_
-              end
-              begin
-                xml.DTM_END_ Date.strptime(link.contract.end_date.to_s, '%Y-%m-%d').strftime('%B')
-                xml.DTD_END_ Date.strptime(link.contract.end_date.to_s, '%Y-%m-%d').strftime('%d')
-                xml.DTY_END_ Date.strptime(link.contract.end_date.to_s, '%Y-%m-%d').strftime('%Y')
-              rescue ArgumentError
-                xml.DTM_END_
-                xml.DTD_END_
-                xml.DTY_END_
-              end
-            }
-          end
+            end
           }
-        }
+        end
+      }
     end
     return builder.to_xml
   end
--- lib/tasks/get_duplicates.rake
+++ lib/tasks/get_duplicates.rake
@@ -4,16 +4,29 @@ namespace :ai_data do
 
   task get_duplicates: :environment do
 
-    college_arr = ['AA', 'AB', 'AG', 'AL', 'BA', 'BC', 'BK', 'CA', 'CM', 'ED',
-                   'EM', 'EN', 'GV', 'HH', 'IST', 'LA', 'LW', 'MD', 'NR', 'SC',
-                   'UC', 'UE', 'UL']
+    college_arr = ['AA', 'AB', 'AG', 'AL', 'BA', 'BC', 'BK', 'CA',
+                   'CM', 'ED', 'EM', 'EN', 'GV', 'HH', 'IST', 'LA',
+                   'LW', 'MD', 'NR', 'SC', 'UC', 'UE', 'UL']
     auth = {:username => "psu/aisupport", :password => "hAeqxpAWubq"}
     xml_arr = []
+    max_retries = 3
     college_arr.each do |college|
-      url = 'https://beta.digitalmeasures.com/login/service/v4/SchemaData/INDIVIDUAL-ACTIVITIES-University/COLLEGE:' + college + '/CONGRANT'
-      #url = 'https://www.digitalmeasures.com/login/service/v4/SchemaData/INDIVIDUAL-ACTIVITIES-University/COLLEGE:' + college + '/CONGRANT'
-      response = HTTParty.get url, :basic_auth => auth
-      #puts response
+      retries = 0
+      #url = 'https://beta.digitalmeasures.com/login/service/v4/SchemaData/INDIVIDUAL-ACTIVITIES-University/COLLEGE:' + college + '/CONGRANT'
+      url = 'https://www.digitalmeasures.com/login/service/v4/SchemaData/INDIVIDUAL-ACTIVITIES-University/COLLEGE:' + college + '/CONGRANT'
+      begin
+        response = HTTParty.get url, :basic_auth => auth
+        puts 'Success'
+      rescue Net::ReadTimeout => e
+        if retries < max_retries
+          puts 'Retrying'
+          retries += 1
+          retry
+        else
+          puts "Exiting script from GET stage.  Max retries reached."
+          exit(1)
+        end
+      end
       xml = Nokogiri::XML.parse(response.to_s)
       xml_arr << xml
     end
@@ -36,13 +49,13 @@ namespace :ai_data do
       v.each do |congrant|
         ospkey_arr << congrant[1]
       end
-      duplicates = ospkey_arr.detect{|e| ospkey_arr.count(e) > 1}
+      duplicates = ospkey_arr.select{|e| ospkey_arr.count(e) > 1}.uniq
       if duplicates
         v.each do |congrant| 
           if duplicates.include?(congrant[1])
             duplicates_final << [k, congrant[0], congrant[1], congrant[2]]
           else
-            puts 'NO DUPLICATES'
+            #puts 'NO DUPLICATES'
           end
         end
       end
@@ -60,7 +73,6 @@ namespace :ai_data do
       end
     end
     wb.write 'data/duplicates.xls'
-
     builder = Nokogiri::XML::Builder.new do |xml|
       xml.Data {
 	xml.CONGRANT {
@@ -72,8 +84,22 @@ namespace :ai_data do
     end
     xml2 = builder.to_xml
     puts xml2
-    url2 = 'https://beta.digitalmeasures.com/login/service/v4/SchemaData:delete/INDIVIDUAL-ACTIVITIES-University'
-    response2 = HTTParty.post url2, :basic_auth => auth, :body => xml2, :headers => {'Content-type' => 'text/xml'}
+    delete_url = 'https://beta.digitalmeasures.com/login/service/v4/SchemaData:delete/INDIVIDUAL-ACTIVITIES-University'
+    retries2 = 0
+    max_retries2 = 3
+    begin
+      response2 = HTTParty.post delete_url, :basic_auth => auth, :body => xml2, :headers => {'Content-type' => 'text/xml'}
+      puts 'Success!'
+    rescue Net::ReadTimeout => e
+      if retries2 < max_retries2
+        puts 'Retrying'
+        retries2 += 1
+        retry
+      else
+        puts "Exiting script from POST stage.  Max retries reached."
+        exit(1)
+      end
+    end
     puts response2
   end
 end
