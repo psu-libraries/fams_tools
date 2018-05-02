f1a981d4b59daa86a1cb5b3308d27d24cea7e8af
Justin Patterson <jrp22@psu.edu>
2018-05-02 12:11:11 -0400

    APPINT-2131 Moved FK to new file and set consistent field type

--- db/migrate/20180316184731_create_contract_faculty_links.rb
+++ db/migrate/20180316184731_create_contract_faculty_links.rb
@@ -1,11 +1,9 @@
 class CreateContractFacultyLinks < ActiveRecord::Migration[5.1]
   def up
     create_table :contract_faculty_links do |t|
-      t.references :contract, foreign_key: true
-      t.references :faculty, foreign_key: true
       t.string :role
       t.integer :pct_credit
-
+      t.bigint :contract_id
     end
   end
   def down
