9bf703bfa7c8023cddb93521b58db56865ec960e
Dan Coughlin <dan.coughlin@gmail.com>
2018-05-23 15:39:06 -0400

    still working on getting routes for downloads

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get :converter, to: 'converter#index', as: :converter_index
+  post :converter, to: 'converter#citation_parse', as: :converter_citation_parse
 end
