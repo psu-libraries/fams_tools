861a14744677adfbca4ca29cb5ebea1d92af0628
Dan Coughlin <dan.coughlin@gmail.com>
2018-05-23 16:21:27 -0400

    added controllers, views for default models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  get 'works/new'
+
+  get 'works/create'
+
+  get 'works/update'
+
+  get 'works/edit'
+
+  get 'works/destroy'
+
+  get 'works/index'
+
+  get 'works/show'
+
+  get 'publication_listings/new'
+
+  get 'publication_listings/create'
+
+  get 'publication_listings/update'
+
+  get 'publication_listings/edit'
+
+  get 'publication_listings/destroy'
+
+  get 'publication_listings/index'
+
+  get 'publication_listings/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get :converter, to: 'converter#index', as: :converter_index
   post :converter, to: 'converter#citation_parse', as: :converter_citation_parse
