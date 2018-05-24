d7b0b04f9ba5026bdf00fd1bfaeb9d94f566b4ae
Dan Coughlin <dan.coughlin@gmail.com>
2018-05-24 09:33:44 -0400

    got downloads to xlsx working for a set of publications/works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -16,6 +16,13 @@ class WorksController < ApplicationController
 
   def index
     @works = Work.where(publication_listing: params[:publication_listing_id])
+    @publication_listing = PublicationListing.find_by_id(params[:publication_listing_id])
+    f = @publication_listing.path
+    xlsx_name = File.basename(f, File.extname(f)) + '.xlsx'
+    respond_to do |format|
+      format.html
+      format.xlsx { render xlsx: 'index', filename: xlsx_name}
+    end
   end
 
   def show
