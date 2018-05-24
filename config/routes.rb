2a02b222c0df3177d3b8a60124b3f1efbbb190ba
Dan Coughlin <dan.coughlin@gmail.com>
2018-05-24 09:34:52 -0400

    removed the converter routes, still need to delete converter files in controller, views

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
   resources :publication_listings do
     resources :works
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get :converter, to: 'converter#index', as: :converter_index
-  post :converter, to: 'converter#citation_parse', as: :converter_citation_parse
 end
