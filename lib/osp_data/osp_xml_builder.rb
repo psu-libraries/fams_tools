7e0a44999db7f2645ce590b35edd9a111395ec8e
Alexander Kiessling <ajkiessl@gmail.com>
2018-06-14 14:32:19 -0400

    Changed xml builders so that access: is 'READ_ONLY'

--- lib/osp_data/osp_xml_builder.rb
+++ lib/osp_data/osp_xml_builder.rb
@@ -25,12 +25,12 @@ class OspXMLBuilder
           xml.Record('username' => faculty.access_id) {
             faculty.contract_faculty_links.each do |link|
               xml.CONGRANT {
-                xml.OSPKEY_ link.contract.osp_key, :access => "LOCKED"
-                xml.BASE_AGREE_ link.contract.base_agreement, :access => "LOCKED"
-                xml.TYPE_ link.contract.grant_contract, :access => "LOCKED"
-                xml.TITLE_ link.contract.title, :access => "LOCKED"
-                xml.SPONORG_ link.contract.sponsor.sponsor_name, :access => "LOCKED"
-                xml.AWARDORG_ link.contract.sponsor.sponsor_type, :access => "LOCKED"
+                xml.OSPKEY_ link.contract.osp_key, :access => "READ_ONLY"
+                xml.BASE_AGREE_ link.contract.base_agreement, :access => "READ_ONLY"
+                xml.TYPE_ link.contract.grant_contract, :access => "READ_ONLY"
+                xml.TITLE_ link.contract.title, :access => "READ_ONLY"
+                xml.SPONORG_ link.contract.sponsor.sponsor_name, :access => "READ_ONLY"
+                xml.AWARDORG_ link.contract.sponsor.sponsor_type, :access => "READ_ONLY"
                 xml.CONGRANT_INVEST {
                   xml.FACULTY_NAME_ faculty.user_id
                   xml.FNAME_ faculty.f_name
@@ -39,35 +39,35 @@ class OspXMLBuilder
                   xml.ROLE_ link.role
                   xml.ASSIGN_ link.pct_credit 
                 }
-                xml.AMOUNT_REQUEST_ link.contract.requested, :access => "LOCKED"
-                xml.AMOUNT_ANTICIPATE_ link.contract.total_anticipated, :access => "LOCKED"
-                xml.AMOUNT_ link.contract.funded, :access => "LOCKED"
-                xml.STATUS_ link.contract.status, :access => "LOCKED"
-                xml.DTM_SUB_ Date.strptime(link.contract.submitted.to_s, '%Y-%m-%d').strftime('%B'), :access => "LOCKED"
-                xml.DTD_SUB_ Date.strptime(link.contract.submitted.to_s, '%Y-%m-%d').strftime('%d'), :access => "LOCKED"
-                xml.DTY_SUB_ Date.strptime(link.contract.submitted.to_s, '%Y-%m-%d').strftime('%Y'), :access => "LOCKED"
+                xml.AMOUNT_REQUEST_ link.contract.requested, :access => "READ_ONLY"
+                xml.AMOUNT_ANTICIPATE_ link.contract.total_anticipated, :access => "READ_ONLY"
+                xml.AMOUNT_ link.contract.funded, :access => "READ_ONLY"
+                xml.STATUS_ link.contract.status, :access => "READ_ONLY"
+                xml.DTM_SUB_ Date.strptime(link.contract.submitted.to_s, '%Y-%m-%d').strftime('%B'), :access => "READ_ONLY"
+                xml.DTD_SUB_ Date.strptime(link.contract.submitted.to_s, '%Y-%m-%d').strftime('%d'), :access => "READ_ONLY"
+                xml.DTY_SUB_ Date.strptime(link.contract.submitted.to_s, '%Y-%m-%d').strftime('%Y'), :access => "READ_ONLY"
                 begin
-                  xml.DTM_AWARD_ Date.strptime(link.contract.awarded.to_s, '%Y-%m-%d').strftime('%B'), :access => "LOCKED"
-                  xml.DTD_AWARD_ Date.strptime(link.contract.awarded.to_s, '%Y-%m-%d').strftime('%d'), :access => "LOCKED"
-                  xml.DTY_AWARD_ Date.strptime(link.contract.awarded.to_s, '%Y-%m-%d').strftime('%Y'), :access => "LOCKED"
+                  xml.DTM_AWARD_ Date.strptime(link.contract.awarded.to_s, '%Y-%m-%d').strftime('%B'), :access => "READ_ONLY"
+                  xml.DTD_AWARD_ Date.strptime(link.contract.awarded.to_s, '%Y-%m-%d').strftime('%d'), :access => "READ_ONLY"
+                  xml.DTY_AWARD_ Date.strptime(link.contract.awarded.to_s, '%Y-%m-%d').strftime('%Y'), :access => "READ_ONLY"
                 rescue ArgumentError
                   xml.DTM_AWARD_
                   xml.DTD_AWARD_
                   xml.DTY_AWARD_
                 end
                 begin
-                  xml.DTM_START_ Date.strptime(link.contract.start_date.to_s, '%Y-%m-%d').strftime('%B'), :access => "LOCKED"
-                  xml.DTD_START_ Date.strptime(link.contract.start_date.to_s, '%Y-%m-%d').strftime('%d'), :access => "LOCKED"
-                  xml.DTY_START_ Date.strptime(link.contract.start_date.to_s, '%Y-%m-%d').strftime('%Y'), :access => "LOCKED"
+                  xml.DTM_START_ Date.strptime(link.contract.start_date.to_s, '%Y-%m-%d').strftime('%B'), :access => "READ_ONLY"
+                  xml.DTD_START_ Date.strptime(link.contract.start_date.to_s, '%Y-%m-%d').strftime('%d'), :access => "READ_ONLY"
+                  xml.DTY_START_ Date.strptime(link.contract.start_date.to_s, '%Y-%m-%d').strftime('%Y'), :access => "READ_ONLY"
                 rescue ArgumentError
                   xml.DTM_START_
                   xml.DTD_START_
                   xml.DTY_START_
                 end
                 begin
-                  xml.DTM_END_ Date.strptime(link.contract.end_date.to_s, '%Y-%m-%d').strftime('%B'), :access => "LOCKED"
-                  xml.DTD_END_ Date.strptime(link.contract.end_date.to_s, '%Y-%m-%d').strftime('%d'), :access => "LOCKED"
-                  xml.DTY_END_ Date.strptime(link.contract.end_date.to_s, '%Y-%m-%d').strftime('%Y'), :access => "LOCKED"
+                  xml.DTM_END_ Date.strptime(link.contract.end_date.to_s, '%Y-%m-%d').strftime('%B'), :access => "READ_ONLY"
+                  xml.DTD_END_ Date.strptime(link.contract.end_date.to_s, '%Y-%m-%d').strftime('%d'), :access => "READ_ONLY"
+                  xml.DTY_END_ Date.strptime(link.contract.end_date.to_s, '%Y-%m-%d').strftime('%Y'), :access => "READ_ONLY"
                 rescue ArgumentError
                   xml.DTM_END_
                   xml.DTD_END_
