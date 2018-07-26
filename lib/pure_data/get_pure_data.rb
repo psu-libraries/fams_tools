b8557fbec0c234bd102b0ff94c715f07ebc502c6
Alexander Kiessling <ajkiessl@gmail.com>
2018-07-26 11:25:19 -0400

    ACTINS-633 Merge conflict resolved

--- lib/pure_data/get_pure_data.rb
+++ lib/pure_data/get_pure_data.rb
@@ -108,7 +108,7 @@ class GetPureData
       noko_obj.xpath('result//contributionToJournal').each do |publication|
         if pure_hash[k]
           pure_hash[k] << {:title => publication.xpath('title').text,
-                           :cattype => publication.xpath('category').text + ' ' + publication.xpath('type').text,
+                           :type => publication.xpath('type').text,
                            :volume => publication.xpath('volume').text,
                            :status => publication.xpath('publicationStatuses//publicationStatus//publicationStatus').text,
                            :dty => publication.xpath('publicationStatuses//publicationDate//year').text,
@@ -130,7 +130,7 @@ class GetPureData
                            :url => publication.xpath('electronicVersions//electronicVersion//doi').text}
         else
           pure_hash[k] =  [{:title => publication.xpath('title').text,
-                           :cattype => publication.xpath('category').text + ' ' + publication.xpath('type').text,
+                           :type => publication.xpath('type').text,
                            :volume => publication.xpath('volume').text,
                            :status => publication.xpath('publicationStatuses//publicationStatus//publicationStatus').text,
                            :dty => publication.xpath('publicationStatuses//publicationDate//year').text,
