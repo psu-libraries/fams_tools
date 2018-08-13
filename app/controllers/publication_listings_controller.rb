0e0f6b64384397db1635e38095191c4d6d967c42
Alexander Kiessling <ajkiessl@gmail.com>
2018-08-13 13:30:40 -0400

    ACTINS-681 Changed the forms in publications listing to take text rather than file.  Now citations can be copied and pasted into the form.

--- app/controllers/publication_listings_controller.rb
+++ app/controllers/publication_listings_controller.rb
@@ -3,25 +3,12 @@ class PublicationListingsController < ApplicationController
   end
 
   def create
-    t_path = ""
-    p_path = ""
 
-    if not params[:training_file].blank?
-      puts "train"
-      t_name = 'tmp_file.txt'
-      t_path = File.join('app', 'parsing_files', t_name)
-      File.open(t_path, "wb") { |f| f.write(params[:training_file].read) }
-      #AnyStyle.parser.train(t_path)
-    end
-
-    p_name = 'tmp_file.txt'
-    p_path = File.join('app', 'parsing_files', p_name)
-    File.open(p_path, "wb") { |f| f.write(params[:publication_file].read) }
-
-    @citations = AnyStyle.parse(p_path)
-    pl = PublicationListing.new(:name => params[:publication_file].original_filename)
+    @citations = AnyStyle.parse(params[:citations].to_s)
+    pl = PublicationListing.new(:name => params[:citations_title])
     pl.save
 
+    puts params[:citations].to_s
     puts @citations
 
     @citations.each_with_index do |item, index|
@@ -53,8 +40,6 @@ class PublicationListingsController < ApplicationController
       work.save
       #puts work
     end
-    File.delete(p_path) if File.exist?(p_path)
-    File.delete(t_path) if File.exist?(t_path)
 
     @publication_listings = PublicationListing.all
     redirect_to publication_listings_url 
