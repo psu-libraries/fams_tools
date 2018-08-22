c41c2114f845f7d1adfa15f3c4a4934c44ab901a
Alexander Kiessling <ajkiessl@gmail.com>
2018-08-22 15:53:38 -0400

    ACTINS-674 Fiddling with responses trying to get them to display.

--- lib/activity_insight/ai_integrate_data.rb
+++ lib/activity_insight/ai_integrate_data.rb
@@ -1,11 +1,12 @@
 class IntegrateData
-  attr_accessor :auth, :url, :batched_xmls
+  attr_accessor :auth, :url, :batched_xmls, :responses
 
   def initialize(xml_builder_obj)
     @auth = {:username => Rails.application.config_for(:activity_insight)[:username],
             :password => Rails.application.config_for(:activity_insight)[:password]}
     @url = 'https://beta.digitalmeasures.com/login/service/v4/SchemaData/INDIVIDUAL-ACTIVITIES-University'
     @batched_xmls = xml_builder_obj.batched_xmls
+    @responses = []
   end
 
   def integrate
@@ -13,12 +14,12 @@ class IntegrateData
     batched_xmls.each do |xml|
       puts xml
       response = HTTParty.post url, :body => xml, :headers => {'Content-type' => 'text/xml'}, :basic_auth => auth, :timeout => 180
-      puts response
       if response.include? 'Error'
         counter += 1
       end
+      responses << response
     end
-    puts counter
+    #puts counter
   end
 
 end
