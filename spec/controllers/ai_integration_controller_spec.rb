1e0bf76768517ae04a35923425ecdab36b3dd9a9
Alexander Kiessling <ajkiessl@gmail.com>
2018-08-28 16:10:31 -0400

    ACTINS-717 added controller spec gem, working on controller specs.  Added fixtures/files to .gitignore.

--- spec/controllers/ai_integration_controller_spec.rb
+++ spec/controllers/ai_integration_controller_spec.rb
@@ -3,7 +3,16 @@ require 'rails_helper'
 RSpec.describe AiIntegrationController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    it "can get the ai_integration page" do
+      get "index"
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "POST #osp_integrate" do 
+    it "can invoke osp integration" do
+      post "osp_integrate", :params => {:congrant_file => fixture_file_upload('files/fake_osp_data.txt'), :ai_backup_file => fixture_file_upload('files/fake_ai_backup_file.txt')}
+      puts response
     end
   end
 
