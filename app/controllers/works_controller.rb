c0c38813c8e6ff222b4b0652683e745fbc29cb01
Alexander Kiessling <ajkiessl@gmail.com>
2018-10-11 15:06:43 -0400

    ACTINS-806 csv output for works.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -19,9 +19,11 @@ class WorksController < ApplicationController
     @publication_listing = PublicationListing.find_by_id(params[:publication_listing_id])
     f = @publication_listing.name
     xlsx_name = File.basename(f, File.extname(f)) + '.xlsx'
+    csv_name = File.basename(f, File.extname(f)) + '.csv'
     respond_to do |format|
       format.html
       format.xlsx { render xlsx: 'index', filename: xlsx_name}
+      format.csv { send_data @works.to_csv}
     end
   end
 
