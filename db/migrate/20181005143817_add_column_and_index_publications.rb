0711a8192e02d008fcd9e4fa163e5651aae672b2
Alexander Kiessling <ajkiessl@gmail.com>
2018-10-12 10:57:04 -0400

    changes to migrations to fix deploy error

--- db/migrate/20181005143817_add_column_and_index_publications.rb
+++ db/migrate/20181005143817_add_column_and_index_publications.rb
@@ -1,8 +1,9 @@
 class AddColumnAndIndexPublications < ActiveRecord::Migration[5.1]
   def change
     add_column :publications, :ai_ids, :string
+    add_column :publications, :pure_ids, :string
     
-    remove_index :publications, :pure_ids
+    remove_index :publications, :pure_id
     add_index :publications, [:pure_ids, :ai_ids], unique: true
   end
 end
