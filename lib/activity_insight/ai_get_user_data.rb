2d8b5735022223a7777a2e882d44c7452ba6eebb
Alexander Kiessling <ajkiessl@gmail.com>
2018-11-08 14:29:44 -0500

    ACTINS-918 Added controller and button for updating user data.

--- lib/activity_insight/ai_get_user_data.rb
+++ lib/activity_insight/ai_get_user_data.rb
@@ -25,12 +25,14 @@ class GetUserData
   def populate_active_users(users_hashed)
     users_hashed.each do |row|
       if row['Enabled?'].downcase == 'yes' && row['Has Access to Manage Activities?'].downcase == 'yes'
-        Faculty.create(access_id: row['Username'].downcase,
-                       user_id:   row['User ID'],
-                       f_name:    row['First Name'],
-                       l_name:    row['Last Name'],
-                       m_name:    row['Middle Name'],
-                       college:   row['College'])
+        faculty = Faculty.find_or_create_by(access_id: row['Username'].downcase)
+
+        faculty.update_attributes({
+          user_id:   row['User ID'],
+          f_name:    row['First Name'],
+          l_name:    row['Last Name'],
+          m_name:    row['Middle Name'],
+          college:   row['College']})
       end
     end
   end
