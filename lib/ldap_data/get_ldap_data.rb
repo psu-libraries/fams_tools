29c4791d9f81550b1535aeff8ad859c636586fce
Alexander Kiessling <ajkiessl@gmail.com>
2018-12-10 17:25:45 -0500

    ACTINS-977 Iterate over faculties.

--- lib/ldap_data/get_ldap_data.rb
+++ lib/ldap_data/get_ldap_data.rb
@@ -1,24 +1,31 @@
 require 'net/ldap'
 
 class GetLdapData
+  attr_accessor :faculty
+
+  def initialize
+    @faculty = Faculty.all
+  end
 
   def call
-    ldap = Net::LDAP.new :host => 'test-dirapps.aset.psu.edu', :port => 389
+    faculty.each do |person|
+      ldap = Net::LDAP.new :host => 'test-dirapps.aset.psu.edu', :port => 389
 
-    filter = Net::LDAP::Filter.eq( "uid", "ajk5603" )
-    treebase = "dc=psu,dc=edu"
+      filter = Net::LDAP::Filter.eq( "uid", "#{person.access_id}" )
+      treebase = "dc=psu,dc=edu"
 
-    ldap.search( :base => treebase, :filter => filter ) do |entry|
-      puts "DN: #{entry.dn}"
-      entry.each do |attribute, values|
-        puts "   #{attribute}:"
-        values.each do |value|
-          puts "      --->#{value}"
+      ldap.search( :base => treebase, :filter => filter ) do |entry|
+        puts "DN: #{entry.dn}"
+        entry.each do |attribute, values|
+          puts "   #{attribute}:"
+          values.each do |value|
+            puts "      --->#{value}"
+          end
         end
       end
-    end
 
-    p ldap.get_operation_result
+      p ldap.get_operation_result
+    end
   end
 end
 
