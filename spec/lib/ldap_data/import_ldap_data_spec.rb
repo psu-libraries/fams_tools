ea21d7a8e2fb104100a84fb4c22f85585cbeeed0
Alexander Kiessling <ajkiessl@gmail.com>
2018-12-11 11:44:17 -0500

    ACTINS-977 Ldap importer started.  Spec built.  Renamed.

---
+++ spec/lib/ldap_data/import_ldap_data_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+require 'ldap_data/import_ldap_data'
+
+RSpec.describe ImportLdapData do
+  
+  before do
+    Faculty.create!(access_id: 'ajk5603')
+  end
+
+  describe '#call' do
+    let(:importer) { ImportLdapData.new }
+
+    it 'imports faculty data from ldap using faculty in our database' do
+      expect(importer.import).to eq(nil)
+    end
+  end
+
+end
