b8bde15a72f2b2f415194f418b441ba778c6bcf2
Alexander Kiessling <ajkiessl@gmail.com>
2019-01-02 14:58:55 -0500

    ACTINS-1011 Navbar specs.

--- spec/integration/main_spec.rb
+++ spec/integration/main_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe MainController do
 
   describe "#update_ai_user_data" do
+
     it "takes psu-users.xls file and updates the user data in the database", type: :feature do
       visit root_path
       expect(page).to have_content("FAMS Tools")
@@ -11,6 +12,34 @@ RSpec.describe MainController do
         page.click_on "Update"
       end
       expect(Faculty.count).to eq(2)
+    end
+  end
+
+  describe "Navbar" do
+
+    it "redirects to CV Parser page", type: :feature do
+      visit root_path
+      expect(page).to have_content("FAMS Tools")
+      within("#collapsibleNavbar") do
+        page.click_on "CV Parser"
+      end
+      expect(current_path).to eq('/publication_listings')
+    end
+
+    it "redirects to Integration page", type: :feature do
+      visit root_path
+      expect(page).to have_content("FAMS Tools")
+      within("#collapsibleNavbar") do
+        page.click_on "Integration"
+      end
+      expect(current_path).to eq('/ai_integration')
+    end
+
+    it "redirects to Main page", type: :feature do
+      visit root_path
+      expect(page).to have_content("FAMS Tools")
+      page.click_on "FAMS Tools"
+      expect(current_path).to eq('/')
     end
   end
 end
