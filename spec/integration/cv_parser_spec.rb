e6aac2c263fde3985cb83bc6a0c15cad6e3d5588
Alexander Kiessling <ajkiessl@gmail.com>
2019-01-02 13:06:54 -0500

    ACTINS-1011 added some lines to test the records created in db.

--- spec/integration/cv_parser_spec.rb
+++ spec/integration/cv_parser_spec.rb
@@ -4,17 +4,20 @@ RSpec.describe PublicationListingsController do
 
   describe "#create" do
 
-    it "takes properly filled out form inputs, parses data, and creates a works record", type: :feature do
+    it "takes properly filled out form inputs, parses data, and creates a publication_listing record with works", type: :feature do
       visit publication_listings_path
       expect(page).to have_content("CV Parser")
       within('#publication_listing_form') do
         page.fill_in 'citations_title', :with => 'Title'
         page.fill_in 'username', :with => 'ajk5603'
         page.select "Book", from: 'contype'
-        page.fill_in 'citations', :with => '1. Billy, B.O.B., Francis, Y.O., & Vern, S.M. (2001). Computers: Are they smart?  Not as smart as me. Penn State University, College of Engineering.'
+        page.fill_in 'citations', :with => '1. Billy, B.O.B., Francis, Y.O., & Vern, S.M. (2001). Computers: Are they smart?  Not as smart as me. Penn State University, College of Engineering.
+2. Billy, B.O.B., Francis, Y.O., & Vern, S.M. (2001). Computers: Are they smart?  Not as smart as me. Penn State University, College of Engineering.'
         click_on "Create Citation Spreadsheet"
       end
-      expect(page).to have_content("Title (1 Records)")
+      expect(PublicationListing.count).to eq(1)
+      expect(Work.count).to eq(2)
+      expect(page).to have_content("Title (2 Records)")
     end
 
     context "when a works record exists" do
