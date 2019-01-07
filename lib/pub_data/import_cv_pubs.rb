ff1d9f90649eae2e4fdbb0ec4de02ecf315cf2db
Alexander Kiessling <ajkiessl@gmail.com>
2019-01-07 14:43:55 -0500

    ACTINS-1024 Working through importer.  Next add columns to complete publication model.

--- lib/pub_data/import_cv_pubs.rb
+++ lib/pub_data/import_cv_pubs.rb
@@ -8,7 +8,39 @@ class ImportCVPubs
   def import_cv_pubs_data
     rows = arrays_to_hashes(data)
     rows.each do |row|
+      faculty = Faculty.find_by(user_id: row["USER_ID"])
 
+      publication = Publication.create!(publication_attributes(row))
+      
+      PublicationFacultyLink.create!(faculty: faculty,
+                                     publication: publication,
+                                     category: row["CONTYPE"])
+
+      external_author = ExternalAuthor.new()
+      counter = 1
+      row.each_key do |key|
+        if key.include? "AUTH"
+          case counter
+          when 1
+            unless row[key].nil?
+              next
+            end
+            counter += 1
+          when 2
+            external_author.f_name = row[key]
+            counter += 1
+          when 3
+            external_author.m_name = row[key]
+            counter += 1
+          when 4
+            external_author.l_name = row[key]
+            external_author.publication = publication
+            external_author.save!
+            external_author = ExternalAuthor.new
+            counter = 1
+          end
+        end
+      end
     end
   end
 
@@ -16,10 +48,18 @@ class ImportCVPubs
 
   def arrays_to_hashes(data)
     headers = data.first
-    data.map! { |n| Hash[ headers.zip(n) ] }
+    data[1..-1].map! { |n| Hash[ headers.zip(n) ] }
   end
 
   def publication_attributes(row)
+    {
+      title: row["TITLE"],
+      volume: row["VOLUME"],
+      edition: row["EDITION"],
+      page_range: row["PAGENUM"],
+      dty: row["DTY_PUB"],
+      journal_title: row["JOURNAL_NAME"]
+    }
   end
 end
 
