e8917ae0eb258c2637a28462f08af75dd7f02c88
Alexander Kiessling <ajkiessl@gmail.com>
2019-01-07 16:12:18 -0500

    ACTINS-1024 Importer and spec completed.  Working on xml builder and integration scripts.

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,6 +1,7 @@
 class Publication < ApplicationRecord
   serialize :pure_ids
   serialize :ai_ids
+  serialize :editors
   has_many :external_authors
   has_many :publication_faculty_links
   has_many :faculties, through: :publication_faculty_links
