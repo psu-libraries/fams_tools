43110ef1668fd9d88e5e99382fd2b4d0a628bcf4
Alexander Kiessling <ajkiessl@gmail.com>
2019-01-07 14:43:55 -0500

    ACTINS-1024 Working through importer.  Next add columns to complete publication model.

--- spec/lib/pub_data/import_cv_pubs_spec.rb
+++ spec/lib/pub_data/import_cv_pubs_spec.rb
@@ -3,10 +3,22 @@ require 'pub_data/import_cv_pubs'
 
 RSpec.describe ImportCVPubs do
 
+  before do
+    Faculty.create( user_id: 123456 )
+  end
+
   describe "#import_cv_pubs_data" do
+
     let(:importer) { ImportCVPubs.new("spec/fixtures/cv_pub.csv") }
+
     it "should parse data from csv and import to database" do
-      expect(importer.import_cv_pubs_data).to change{ Publication.count }.by 2
+      expect{importer.import_cv_pubs_data}.to change{ Publication.count }.by 2
+      expect(ExternalAuthor.count).to eq 2
+      
+      p1 = Publication.first
+      p2 = Publication.second
+
+      expect(p1.title).to eq("Test Title 1")
     end
   end
 end
