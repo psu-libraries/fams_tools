c4e0db373bd91da58a86fbce7cca88db20904696
Alexander Kiessling <ajkiessl@gmail.com>
2019-01-10 09:54:40 -0500

    ACTINS-1040 contract_faculty_link completed

---
+++ spec/models/contract_faculty_link_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe ContractFacultyLink, type: :model do
+  describe 'columns' do
+    it { is_expected.to have_db_column(:id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:role).of_type(:string) }
+    it { is_expected.to have_db_column(:pct_credit).of_type(:integer) }
+    it { is_expected.to have_db_column(:contract_id).of_type(:integer) }
+    it { is_expected.to have_db_column(:faculty_id).of_type(:integer) }
+
+    it { is_expected.to have_db_index(:faculty_id) }
+    it { is_expected.to have_db_index([:contract_id, :faculty_id]).unique(true) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:faculty) }
+    it { is_expected.to belong_to(:contract) }
+  end
+end
