0fb2e5fc73b96da56b753e73dba590e6c0f80b68
Alexander Kiessling <ajkiessl@gmail.com>
2019-01-10 10:08:27 -0500

    ACTINS-1040 course completed

---
+++ spec/models/course_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe Course, type: :model do
+  describe 'columns' do
+    it { is_expected.to have_db_column(:id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:academic_course_id).of_type(:integer) }
+    it { is_expected.to have_db_column(:term).of_type(:string) }
+    it { is_expected.to have_db_column(:calendar_year).of_type(:integer) }
+    it { is_expected.to have_db_column(:course_short_description).of_type(:string) }
+    it { is_expected.to have_db_column(:course_long_description).of_type(:text) }
+
+    it { is_expected.to have_db_index([:academic_course_id, :term, :calendar_year]).unique(true) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:faculties) }
+    it { is_expected.to have_many(:sections) }
+  end
+end
+
