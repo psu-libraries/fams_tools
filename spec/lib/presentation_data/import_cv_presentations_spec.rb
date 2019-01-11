b9af9ec32e604883819722d4ce012bc55f454013
Alexander Kiessling <ajkiessl@gmail.com>
2019-01-11 10:57:30 -0500

    ACTINS-1052 Fixture added, spec set up, importer setup.  Working on model and migration.

---
+++ spec/lib/presentation_data/import_cv_presentations_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+require 'pub_data/import_cv_presentations'
+
+RSpec.describe ImportCVPresentations do
+
+  before do
+    Faculty.create( user_id: 123456 )
+  end
+
+  describe "#import_cv_presentations_data" do
+
+    let(:importer) { ImportCVPresentations.new("spec/fixtures/cv_presentation.csv") }
+
+    it "should parse data from csv and import to database" do
+      expect{importer.import_cv_presentations_data}.to change{ Presentation.count }.by 2
+      
+      p1 = Presentation.first
+      p2 = Presentation.second
+    end
+  end
+end
+
