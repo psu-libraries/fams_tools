cb5670427ad1b86ab00610d10105ba0f67dc7778
Alex Kiessling <ajkiessl@gmail.com>
2020-02-20 17:03:15 -0500

    ACTINS-1960 Gathering access_ids for any integration that returns an error.  Gathering osp keys for osp integrations that return an error.  Logging when integration integrates.  Specs to check for all of this.

--- app/jobs/cv_pub_integrate_job.rb
+++ app/jobs/cv_pub_integrate_job.rb
@@ -2,6 +2,7 @@ class CvPubIntegrateJob < ApplicationJob
 
   def perform(params, log_path)
     error_logger = Logger.new("public/#{log_path}")
+    error_logger.info "CV Publications Integration to #{params[:target]} initiated at: #{DateTime.now}"
     f_name = params[:cv_pub_file].original_filename
     f_path = File.join('app', 'parsing_files', f_name)
     File.open(f_path, "wb") { |f| f.write(params[:cv_pub_file].read) }
@@ -10,7 +11,7 @@ class CvPubIntegrateJob < ApplicationJob
     my_integrate = IntegrateData.new(PubXMLBuilder.new, params[:target])
     errors = my_integrate.integrate
     File.delete(f_path) if File.exist?(f_path)
-    error_logger.info "Errors for CV Publications Integration to #{params[:target]} on: #{DateTime.now}"
+    error_logger.info "Errors for CV Publications Integration to #{params[:target]} at: #{DateTime.now}"
     error_logger.error errors
   end
 end
