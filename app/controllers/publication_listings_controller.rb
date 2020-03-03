1d9cd99e4e294cd6e83fce3ea262d187c15d7d06
Alex Kiessling <ajkiessl@gmail.com>
2020-03-03 16:38:08 -0500

    ACTINS-2015 Cleaned up publication listings controller.

--- app/controllers/publication_listings_controller.rb
+++ app/controllers/publication_listings_controller.rb
@@ -1,60 +1,25 @@
 class PublicationListingsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
-
-    @citations = AnyStyle.parse(params[:citations].to_s)
-    pl = PublicationListing.new(:name => params[:citations_title])
+    pl = PublicationListing.new(name: params[:citations_title])
     pl.save
-
-    @citations.each_with_index do |item, index|
+    citations.each do |item|
       work = Work.new(
-          :username => params[:username],
-          :author => item[:author]&.collect { |e| [split_name(e[:given]), e[:family]].flatten }, 
-          :title => item.dig(:title, 0), 
-          :journal => item.dig(:journal, 0),
-          :volume => item.dig(:volume, 0),
-          :edition => item.dig(:edition, 0),
-          :pages => item.dig(:pages, 0),
-          :year => year(item),
-          :month => month(item),
-          :day => day(item),
-          :booktitle => item.dig(:booktitle, 0),
-          :container => item.dig(:"container-title", 0),
-          :contype => params[:contype],
-          :genre => item[:type],
-          :doi => item.dig(:doi, 0),
-          :editor => item[:editor]&.collect {|e| "#{e[:given]} #{e[:family]}"},
-          :institution => item.dig(:institution, 0),
-          :isbn => item.dig(:isbn, 0), 
-          :location => item.dig(:location, 0),
-          :note => item.dig(:note, 0),
-          :publisher => item.dig(:publisher, 0),
-          :retrieved => item.dig(:retrieved, 0),
-          :tech => item.dig(:tech, 0),
-          :translator => item.dig(:translator, 0),
-          :unknown => item.dig(:unknown, 0),
-          :url => item.dig(:url, 0),
-          :publication_listing => pl
+        work_attrs(item, pl)
       )
       work.save
-      #puts work
     end
-
-    @publication_listings = PublicationListing.all
-    redirect_to publication_listings_url 
+    redirect_to publication_listings_url
   end
 
-  def update
-  end
+  def update; end
 
-  def edit
-  end
+  def edit; end
 
   def destroy
-    @publication_listing = PublicationListing.find(params[:publication_listing_id])
-    @publication_listing.destroy
+    pl = PublicationListing.find(params[:publication_listing_id])
+    pl.destroy
 
     flash[:notice] = 'Publication listing successfully deleted.'
     redirect_to publication_listings_path
@@ -64,10 +29,47 @@ class PublicationListingsController < ApplicationController
     @publication_listings = PublicationListing.all
   end
 
-  def show
-  end
+  def show; end
 
   private
+
+  def citations
+    AnyStyle.parse(params[:citations].to_s)
+  end
+
+  def work_attrs(item, publication_listing)
+    {
+      username: params[:username],
+      author: item[:author]&.collect do |e|
+        [split_name(e[:given]), e[:family]].flatten
+      end,
+      title: item.dig(:title, 0),
+      journal: item.dig(:journal, 0),
+      volume: item.dig(:volume, 0),
+      edition: item.dig(:edition, 0),
+      pages: item.dig(:pages, 0),
+      year: year(item),
+      month: month(item),
+      day: day(item),
+      booktitle: item.dig(:booktitle, 0),
+      container: item.dig(:'container-title', 0),
+      contype: params[:contype],
+      genre: item[:type],
+      doi: item.dig(:doi, 0),
+      editor: item[:editor]&.collect { |e| "#{e[:given]} #{e[:family]}" },
+      institution: item.dig(:institution, 0),
+      isbn: item.dig(:isbn, 0),
+      location: item.dig(:location, 0),
+      note: item.dig(:note, 0),
+      publisher: item.dig(:publisher, 0),
+      retrieved: item.dig(:retrieved, 0),
+      tech: item.dig(:tech, 0),
+      translator: item.dig(:translator, 0),
+      unknown: item.dig(:unknown, 0),
+      url: item.dig(:url, 0),
+      publication_listing: publication_listing
+    }
+  end
 
   def year(item)
     date_held = split_date(item)
@@ -85,14 +87,16 @@ class PublicationListingsController < ApplicationController
   end
 
   def split_date(item)
-    item.dig(:date, 0).present? ?  item.dig(:date, 0).split('-') : nil
+    item.dig(:date, 0).present? ? item.dig(:date, 0).split('-') : nil
   end
 
   def split_name(name)
-    if name&.split(/(?<=[. ])/)&.length == 2
-      [name&.split(/(?<=[. ])/)[0].strip, name&.split(/(?<=[. ])/)[1].strip]
+    return name if name.empty?
+
+    if name.split(/(?<=[. ])/).length == 2
+      [name.split(/(?<=[. ])/)[0].strip, name.split(/(?<=[. ])/)[1].strip]
     else
-      [name, ""]
+      [name, '']
     end
   end
 end
