f56eddcdc9e7ee7b6ee92cd5df94c503b9ba784a
Alex Kiessling <ajkiessl@gmail.com>
2020-03-03 16:08:09 -0500

    ACTINS-2015 Fix ai_integration_controller so the specs work.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,7 @@
 require 'activity_insight/ai_get_user_data'
 
 class MainController < ApplicationController
-
   def update_ai_user_data
-    psu_users_name = params[:psu_users_file].original_filename
-    psu_users_path = File.join('app', 'parsing_files', psu_users_name)
     File.delete(psu_users_path) if File.exist?(psu_users_path)
     File.open(psu_users_path, "wb") { |f| f.write(params[:psu_users_file].read) }
     my_get_user_data = GetUserData.new(psu_users_path)
@@ -17,4 +14,10 @@ class MainController < ApplicationController
   def index
   end
 
+  private
+
+  def psu_users_path
+    psu_users_name = params[:psu_users_file].original_filename
+    File.join('app', 'parsing_files', psu_users_name)
+  end
 end
