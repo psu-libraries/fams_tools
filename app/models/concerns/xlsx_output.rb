0f114ffbbd1a60e4e3dae9bb9779c01f1b9c2430
Alex Kiessling <ajkiessl@gmail.com>
2020-03-05 14:37:10 -0500

    ACTINS-2015 Major architecture changes/refactoring for works outputs.

---
+++ app/models/concerns/xlsx_output.rb
@@ -0,0 +1,10 @@
+class XLSXOutput < SpreadsheetOutput
+  def output(axlsx_workbook)
+    axlsx_workbook.add_worksheet(:name => "citations") do |sheet|
+      sheet.add_row formatted_headers
+      works.each do |item|
+        sheet.add_row formatted_row(item)
+      end
+    end
+  end
+end
