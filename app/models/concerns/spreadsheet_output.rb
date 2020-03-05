c00e9c578d354ac483c6fa94cad9ae9fe6395718
Alex Kiessling <ajkiessl@gmail.com>
2020-03-05 16:26:13 -0500

    ACTINS-2015-models Refactored models and models/concern with the help of rubocop.

--- app/models/concerns/spreadsheet_output.rb
+++ app/models/concerns/spreadsheet_output.rb
@@ -42,11 +42,11 @@ class SpreadsheetOutput < WorkOutputs
   def longest
     num = 0
     works.each do |item|
-      unless item[:author].nil?
-        if num < item[:author].length + header_length
-          num = item[:author].length + header_length
-        end
-      end
+      next if item[:author].nil?
+
+      next unless num < item[:author].length + header_length
+
+      num = item[:author].length + header_length
     end
     num
   end
@@ -54,38 +54,56 @@ class SpreadsheetOutput < WorkOutputs
   def formatted_pres_headers(header_item)
     counter = longest - header_length
     while header_item.length < longest
-      header_item.insert(header_length, ["PRESENT_AUTH_#{counter}_FACULTY_NAME", "PRESENT_AUTH_#{counter}_FNAME", "PRESENT_AUTH_#{counter}_MNAME", "PRESENT_AUTH_#{counter}_LNAME"])
+      header_item.insert(header_length, present_auth_headers(counter))
       counter -= 1
     end
     header_item.flatten
+  end
+
+  def present_auth_headers(counter)
+    %W[PRESENT_AUTH_#{counter}_FACULTY_NAME PRESENT_AUTH_#{counter}_FNAME
+       PRESENT_AUTH_#{counter}_MNAME PRESENT_AUTH_#{counter}_LNAME]
   end
 
   def formatted_pub_headers(header_item)
     counter = longest - header_length
     while header_item.length < longest
-      header_item.insert(header_length, ["INTELLCONT_AUTH_#{counter}_FACULTY_NAME", "INTELLCONT_AUTH_#{counter}_FNAME", "INTELLCONT_AUTH_#{counter}_MNAME", "INTELLCONT_AUTH_#{counter}_LNAME"])
+      header_item.insert(header_length, intellcont_auth_headers(counter))
       counter -= 1
     end
     header_item.flatten
   end
 
+  def intellcont_auth_headers(counter)
+    %W[INTELLCONT_AUTH_#{counter}_FACULTY_NAME INTELLCONT_AUTH_#{counter}_FNAME
+       INTELLCONT_AUTH_#{counter}_MNAME INTELLCONT_AUTH_#{counter}_LNAME]
+  end
+
   def formatted_headers
     headers_cleaned = stripped_headers(marked_empty_headers)
-    return formatted_pres_headers(headers_cleaned) if workstype == 'presentations'
+    if workstype == 'presentations'
+      return formatted_pres_headers(headers_cleaned)
+    end
 
     formatted_pub_headers(headers_cleaned)
   end
 
   def row(item)
-    [
-        item[:username], Faculty.find_by(access_id: item[:username])&.user_id,
-        item[:title], item[:journal], item[:volume], item[:edition],
-        item[:pages], item[:year], item[:month], item[:day], item[:booktitle],
-        item[:container], item[:contype], item[:doi], item[:editor]&.join(", "),
-        item[:institution], item[:isbn], item[:location], item[:note],
-        item[:publisher], item[:retrieved], item[:tech], item[:translator],
-        item[:unknown], item[:url]
-    ]
+    row_arr = []
+    HEADER_MAP.each do |key|
+      row_arr << row_assign(item, key)
+    end
+    row_arr
+  end
+
+  def row_assign(item, key)
+    if key == :ignore
+      return Faculty.find_by(access_id: item[:username])&.user_id
+    end
+
+    return item[key]&.join(', ') if key == :editor
+
+    item[key]
   end
 
   def formatted_row(item)
@@ -116,24 +134,40 @@ class SpreadsheetOutput < WorkOutputs
 
   def format_author_data(row_item, item)
     item[:author]&.reverse&.each do |author|
-      if cv_owner.present? && author[2]&.upcase == cv_owner&.l_name&.upcase && author[0][0]&.upcase == cv_owner&.f_name[0]&.upcase
-        row_item.insert(header_length, [cv_owner&.user_id, author[0], author[1], author[2]])
-      else
-        row_item.insert(header_length, ["", author[0], author[1], author[2]])
-      end
+      insert_authors(row_item, author)
     end
     match_length_author(row_item, item)
   end
 
+  def insert_authors(row_item, author)
+    if can_insert_owner(author)
+      row_item.insert(header_length, [cv_owner&.user_id, author[0],
+                                      author[1], author[2]])
+    else
+      row_item.insert(header_length, ['', author[0], author[1], author[2]])
+    end
+  end
+
+  def can_insert_owner(author)
+    cv_owner.present? && author_is_owner(author)
+  end
+
+  def author_is_owner(author)
+    owner_f_name = cv_owner&.f_name
+    cond1 = author[2]&.upcase == cv_owner&.l_name&.upcase
+    cond2 = author[0][0]&.upcase == owner_f_name[0]&.upcase
+    cond1 && cond2
+  end
+
   def match_length_author(row_item, item)
     while row_item.length < longest
-      row_item.insert(header_length + item[:author].length, ["", "", "", ""])
+      row_item.insert(header_length + item[:author].length, ['', '', '', ''])
     end
   end
 
   def match_length(row_item)
     while row_item.length < longest
-      row_item.insert(header_length, ["", "", "", ""])
+      row_item.insert(header_length, ['', '', '', ''])
     end
   end
 end
--- app/models/concerns/xlsx_output.rb
+++ app/models/concerns/xlsx_output.rb
@@ -1,6 +1,6 @@
 class XLSXOutput < SpreadsheetOutput
   def output(axlsx_workbook)
-    axlsx_workbook.add_worksheet(:name => "citations") do |sheet|
+    axlsx_workbook.add_worksheet(name: 'citations') do |sheet|
       sheet.add_row formatted_headers
       works.each do |item|
         sheet.add_row formatted_row(item)
