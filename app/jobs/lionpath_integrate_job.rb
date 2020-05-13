a9e042a5e19ba730593755f1792d3dd7e2e519a3
Alex Kiessling <ajkiessl@gmail.com>
2020-05-13 14:02:05 -0400

    ACTINS-2088 Refactored Lionpath integrate job

--- app/jobs/lionpath_integrate_job.rb
+++ app/jobs/lionpath_integrate_job.rb
@@ -1,26 +1,55 @@
 class LionpathIntegrateJob < ApplicationJob
-
   # When using rake task, file_exist must = true
-  # This will use existing file 'app/parsing_files/courses_taught.csv' instead of uploaded file
-  def perform(params, log_path, file_exist=false)
+  # This will use existing file 'app/parsing_files/courses_taught.csv'
+  # instead of uploaded file
+  def perform(params, log_path, file_exist = false)
     error_logger = Logger.new("public/#{log_path}")
-    error_logger.info "Courses Taught Integration to #{params[:target]} initiated at: #{DateTime.now}"
+    error_logger.info info_message(params[:target])
+    f_path = create_tmp_file(params, file_exist)
+    populate_course_data(f_path)
+    errors = integrate_course_data(params[:target])
+    File.delete(f_path) if File.exist?(f_path)
+    error_logger.info error_message(params[:target])
+    error_logger.error errors
+  end
+
+  private
+
+  def create_tmp_file(params, file_exist)
     if file_exist == false
       f_name = params[:courses_file].original_filename
       f_path = File.join('app', 'parsing_files', f_name)
-      File.open(f_path, "wb") { |f| f.write(params[:courses_file].read) }
+      File.open(f_path, 'wb') { |f| f.write(params[:courses_file].read) }
+      f_path
     else
       # Running bash script to grab lionpath files
       `#{Rails.root}/bin/courses-taught.sh`
-      f_path = File.join('app', 'parsing_files', 'courses_taught.csv')
+      File.join('app', 'parsing_files', 'courses_taught.csv')
     end
-    my_lionpath_populate = LionPathPopulateDB.new(LionPathParser.new(filepath = f_path))
-    my_lionpath_populate.format_and_filter
-    my_lionpath_populate.populate
-    lionpath_integrate = IntegrateData.new(LionPathXMLBuilder.new, params[:target])
-    errors = lionpath_integrate.integrate
-    File.delete(f_path) if File.exist?(f_path)
-    error_logger.info "Errors for Courses Taught Integration to #{params[:target]} at: #{DateTime.now}"
-    error_logger.error errors
+  end
+
+  def populate_course_data(f_path)
+    lionpath_populate_obj(f_path).format_and_filter
+    lionpath_populate_obj(f_path).populate
+  end
+
+  def lionpath_populate_obj(f_path)
+    LionPathPopulateDB.new(LionPathParser.new(f_path))
+  end
+
+  def integrate_course_data(target)
+    lionpath_integrate_obj(target).integrate
+  end
+
+  def lionpath_integrate_obj(target)
+    IntegrateData.new(LionPathXMLBuilder.new, target)
+  end
+
+  def error_message(target)
+    "Errors for Courses Taught Integration to #{target} at: #{DateTime.now}"
+  end
+
+  def info_message(target)
+    "Courses Taught Integration to #{target} initiated at: #{DateTime.now}"
   end
 end
