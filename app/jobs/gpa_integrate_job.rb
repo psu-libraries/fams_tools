a3184875c6024a27ee70817ce4d91c27190e7a82
Alex Kiessling <ajkiessl@gmail.com>
2020-06-22 13:55:20 -0400

    ACTINS-2169 refactored jobs and added request spec

--- app/jobs/gpa_integrate_job.rb
+++ app/jobs/gpa_integrate_job.rb
@@ -1,8 +1,6 @@
 class GpaIntegrateJob < ApplicationJob
 
-  def perform(params, log_path)
-    error_logger = Logger.new("public/#{log_path}")
-    error_logger.info "GPA Integration to #{params[:target]} initiated at: #{DateTime.now}"
+  def integrate(params)
     f_name = params[:gpa_file].original_filename
     f_path = File.join('app', 'parsing_files', f_name)
     File.open(f_path, "wb") { |f| f.write(params[:gpa_file].read) }
@@ -12,7 +10,12 @@ class GpaIntegrateJob < ApplicationJob
     gpa_integration = IntegrateData.new(gpa_xml_builder, params[:target])
     errors = gpa_integration.integrate
     File.delete(f_path) if File.exist?(f_path)
-    error_logger.info "Errors for GPA Integration to #{params[:target]} at: #{DateTime.now}"
-    error_logger.error errors
+    errors
+  end
+
+  private
+
+  def name
+    'GPA Integration'
   end
 end
