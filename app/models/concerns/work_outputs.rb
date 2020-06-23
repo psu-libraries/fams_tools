2def7e8ec5076a38bbed5bd91855cf598609de43
Alex Kiessling <ajkiessl@gmail.com>
2020-06-23 13:41:17 -0400

    ACTINS-2199 Removed pagenum from presentations spreadsheet output

--- app/models/concerns/work_outputs.rb
+++ app/models/concerns/work_outputs.rb
@@ -10,18 +10,17 @@ class WorkOutputs
   end
 
   # These "maps" map the data parsed from the cv parser to the headers in the spreadsheet outputs
-  PRES_MAP = %i[username title journal edition pages
-                  year month day booktitle container contype doi
-                  editor institution isbn location note publisher retrieved
-                  tech translator unknown url].freeze
+  PRES_MAP = %i[username title journal edition year month day booktitle
+                container contype doi editor institution isbn location
+                note publisher retrieved tech translator unknown url].freeze
 
   PUB_MAP = %i[username title journal volume edition pages
                   year month day booktitle container contype doi
                   editor institution isbn location note publisher retrieved
                   tech translator unknown url].freeze
 
-  PRES_HEADERS = %w[USERNAME TITLE journal EDITION PAGENUM
-                    DTY_END DTM_END DTD_END booktitle NAME TYPE doi editor
+  PRES_HEADERS = %w[USERNAME TITLE journal EDITION DTY_END DTM_END
+                    DTD_END booktitle NAME TYPE doi editor
                     ORG isbn LOCATION COMMENT publisher retrieved
                     tech translator unknown url].freeze
 
