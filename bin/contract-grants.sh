c05a965a2523759f2111702f551926d4a3f8c59b
Alex Kiessling <ajkiessl@gmail.com>
2020-08-28 16:22:28 -0400

    ACTINS-2170 Changed some wording and oganization of contract-grants.sh.  Added psu-users.sh for pulling in psu-users.xls

--- bin/contract-grants.sh
+++ bin/contract-grants.sh
@@ -1,5 +1,5 @@
-DM_USERNAME=$1
-DM_PASSWORD=$2
+BACKUPS_USERNAME=$1
+BACKUPS_PASSWORD=$2
 
 #!/bin/bash
 
@@ -7,5 +7,6 @@ DM_PASSWORD=$2
 wget https://service.sims.psu.edu/digitalmeasures/dmresults.csv -O app/parsing_files/contract_grants.csv
 
 # Grab CONGRANT.csv from Digital Measures
-wget --user $DM_USERNAME --password $DM_PASSWORD https://webservices.digitalmeasures.com/login/service/v4/SchemaData:backup/INDIVIDUAL-ACTIVITIES-University -O app/parsing_files/backups.zip
-unzip -d app/parsing_files/ app/parsing_files/backups.zip
+wget --user $BACKUPS_USERNAME --password $BACKUPS_PASSWORD \
+https://webservices.digitalmeasures.com/login/service/v4/SchemaData:backup/INDIVIDUAL-ACTIVITIES-University \
+-O app/parsing_files/backups.zip unzip -d app/parsing_files/ app/parsing_files/backups.zip
---
+++ bin/psu-users.sh
@@ -0,0 +1,17 @@
+DM_USERNAME=$1
+DM_PASSWORD=$2
+
+#!/bin/bash
+
+# Initial wget to authenticate and create cookies.txt file with session info
+wget --save-cookies app/parsing_files/cookies.txt --keep-session-cookie --delete-after \
+--post-data "username=$DM_USERNAME&password=$DM_PASSWORD" \
+'https://www.digitalmeasures.com/login/psu/admin/authentication/authenticatePassword.do'
+
+# Second curl to get psu-users.xls using cookies.txt with session info
+curl -b app/parsing_files/cookies.txt \
+'https://www.digitalmeasures.com/login/psu/admin/security/management/user/downloadUsers.do?selectedUsers=345' \
+>> app/parsing_files/psu-users.xls
+
+# Delete cookies.txt
+rm app/parsing_files/cookies.txt
