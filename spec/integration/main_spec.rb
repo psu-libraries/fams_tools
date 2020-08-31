102c24c0184e48ab058e92f176d32c8b4546de32
Alex Kiessling <ajkiessl@gmail.com>
2020-08-31 10:20:58 -0400

    removed test for GUI update of users

--- spec/integration/main_spec.rb
+++ spec/integration/main_spec.rb
@@ -2,20 +2,6 @@ require 'rails_helper'
   
 RSpec.describe MainController do
 
-  describe "#update_ai_user_data" do
-
-    it "takes psu-users.xls file and updates the user data in the database", type: :feature do
-      visit root_path
-      expect(page).to have_content("FAMS Tools")
-      within("#psu-users") do
-        page.attach_file 'psu_users_file', Rails.root.join('spec/fixtures/users.xls')
-        page.click_on "Update"
-      end
-      expect(Faculty.count).to eq(2)
-      expect(page).to have_content("Update successful!")
-    end
-  end
-
   describe "Navbar" do
 
     it "redirects to CV Parser page", type: :feature do
