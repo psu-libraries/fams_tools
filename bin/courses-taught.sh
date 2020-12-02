30e28b12cc673ed04ae7dd34498631d9109fb9bd
Alex Kiessling <ajkiessl@gmail.com>
2020-12-02 15:48:43 -0500

    just pulling full list of files in ftp directory.  back to using production for lionpath integration

--- bin/courses-taught.sh
+++ bin/courses-taught.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Using SFTP connection with Lionpath host, pull 4 most recent files
-OUTPUT=$(sftp -P 22 -b lp_sftp_newest.bat -i ~/.ssh/id_rsa_lionpath_prod uldsrdc@prod-nfs.lionpath.psu.edu | head -10)
+# Using SFTP connection with Lionpath host to pull file names in order of most to least recent
+OUTPUT=$(sftp -P 22 -b bin/lp_sftp_newest.bat -i ~/.ssh/id_rsa_lionpath_prod uldsrdc@prod-nfs.lionpath.psu.edu)
 
 # Single out the first PE_RP_ACTIVITY_INSIGHT and pull this down as app/parsing_files/courses_taught.csv
 for x in $OUTPUT
 do
-  echo $x
   if [[ "$x" =~ "PE_RP_ACTIVITY_INSIGHT" ]]; then
     sftp -P 22 -r -i ~/.ssh/id_rsa_lionpath_prod uldsrdc@prod-nfs.lionpath.psu.edu:/out/$x app/parsing_files/courses_taught.csv
     break
