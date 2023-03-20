9a6f4afc3af7a670c9a1ff582edfdd68d36c2dd9
Alex Kiessling <ajkiessl@gmail.com>
2023-03-20 12:08:52 -0400

    Remaining security vulns

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.16.0"
+lock "~> 3.17.1"
 
 # Set assets roles to occur on jobs as well as web
 set :assets_role, [:web]
