7909dd563867c8a794ffd49876f2dd8f5a5c7f80
ddecollo <dad5925@psu.edu>
2023-07-27 17:14:51 -0400

    Update import specs for ComEffort and ComQuality

--- spec/importers/activity_insight/get_user_data_spec.rb
+++ spec/importers/activity_insight/get_user_data_spec.rb
@@ -3,15 +3,15 @@ require 'importers/importers_helper'
 RSpec.describe ActivityInsight::GetUserData do
 
   let!(:faculty1) { FactoryBot.create :faculty, access_id: 'xxx111' }
-  
+
   let(:fake_book) do
     book = Spreadsheet::Workbook.new
     sheet = book.create_worksheet
     sheet.row(1).replace []
     sheet.row(2).replace ['Last Name', 'First Name', 'Middle Name', 'Email', 'Username', 'User ID', 'PSU ID #', 'Enabled?', 'Has Access to Manage Activities?',
-                          'Date Created', 'Campus', 'Campus Name', 'College', 'College Name', 'Department', 'Division', 'Institute', 'School', 'Security']
-    sheet.row(3).replace ['X', 'Bill', 'X', 'X', 'zzz999', '123', 'X', 'Yes', 'Yes', 'X', 'UP', 'X', 'BA', 'X', 'X', 'X', 'X', 'X', 'X']
-    sheet.row(4).replace ['X', 'Jimmy', 'X', 'X', 'xxx111', '321', 'X', 'No', 'No', 'X', 'UP', 'X', 'AG', 'X', 'X', 'X', 'X', 'X', 'X']
+                          'Date Created', 'Campus', 'Campus Name', 'College', 'College Name', 'Department', 'Division', 'Institute', 'School', 'Security', 'Penn State Health Username']
+    sheet.row(3).replace ['X', 'Bill', 'X', 'X', 'zzz999', '123', 'X', 'Yes', 'Yes', 'X', 'UP', 'X', 'MD', 'X', 'X', 'X', 'X', 'X', 'X', 'abc1234']
+    sheet.row(4).replace ['X', 'Jimmy', 'X', 'X', 'xxx111', '321', 'X', 'No', 'No', 'X', 'UP', 'X', 'AG', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
     sheet
   end
 
@@ -24,8 +24,9 @@ RSpec.describe ActivityInsight::GetUserData do
       get_user_data_obj.call
       expect(Faculty.all.count).to eq(1)
       expect(Faculty.find_by(access_id: 'zzz999').f_name).to eq('Bill')
-      expect(Faculty.find_by(access_id: 'zzz999').college).to eq('BA')
+      expect(Faculty.find_by(access_id: 'zzz999').college).to eq('MD')
       expect(Faculty.find_by(access_id: 'zzz999').campus).to eq('UP')
+      expect(Faculty.find_by(access_id: 'zzz999').com_id).to eq('abc1234')
     end
   end
 
---
+++ spec/importers/com_data/com_effort_xml_builder_spec.rb
@@ -0,0 +1,75 @@
+require 'importers/importers_helper'
+
+RSpec.describe ComData::ComEffortXmlBuilder do
+
+  let(:data_sets) do
+    [{'FACULTY_USERNAME' => 'lskywalker',
+      'FACULTY_NAME' => 'Skywalker Luke',
+      'COURSE' => 'the Force',
+      'COURSE_YEAR' => '1976-1977',
+      'EVENT_TYPE' => 'Lecture',
+      'EVENT' => 'FTF REQ Various Rooms 10-12 PBL - EndoRepro PBL 1402 - Thyroid',
+      'EVENT_DATE' => '5/25/77 10:00',
+      'UME_CALCULATED_TEACHING_WHILE_NON_BILLING_EFFORT__HRS_' => 2},
+      {'FACULTY_USERNAME' => 'hgranger',
+      'FACULTY_NAME' => 'Granger Hermione',
+      'COURSE' => 'Potions',
+      'COURSE_YEAR' => '1997-1998',
+      'EVENT_TYPE' => 'Sm Grp Facilitation',
+      'EVENT' => 'FTF REQ Various Rooms 10-12 PBL - EndoRepro PBL 1402 - Thyroid',
+      'EVENT_DATE' => '6/26/97 9:45',
+      'UME_CALCULATED_TEACHING_WHILE_NON_BILLING_EFFORT__HRS_' => 7}
+    ]
+  end
+
+  let(:xml_builder_obj) {ComData::ComEffortXmlBuilder.new}
+
+  describe '#build_xml_effort' do
+    it 'should return a list with an xml of INSTRUCT_TAUGHT records' do
+      data_sets.each do |row|
+        faculty = FactoryBot.create :faculty, com_id: row['FACULTY_USERNAME'], college: 'MD'
+        begin
+          ComEffort.create( com_id:         row['FACULTY_USERNAME'],
+                            course_year:    row['COURSE_YEAR'],
+                            course:         row['COURSE'],
+                            event_type:     row['EVENT_TYPE'],
+                            faculty_name:   row['FACULTY_NAME'],
+                            event:          row['EVENT'],
+                            event_date:     row['EVENT_DATE'],
+                            hours:          row['UME_CALCULATED_TEACHING_WHILE_NON_BILLING_EFFORT__HRS_'],
+                            faculty:        faculty)
+        end
+      end
+      expect(xml_builder_obj.xmls_enumerator_effort.first).to eq(
+'<?xml version="1.0" encoding="UTF-8"?>
+<Data>
+  <Record PennStateHealthUsername="lskywalker">
+    <INSTRUCT_TAUGHT>
+      <COURSE_YEAR access="READ_ONLY">1976-1977</COURSE_YEAR>
+      <COURSE_TITLE access="READ_ONLY">the Force</COURSE_TITLE>
+      <EVENT_TITLE access="READ_ONLY">FTF REQ Various Rooms 10-12 PBL - EndoRepro PBL 1402 - Thyroid</EVENT_TITLE>
+      <EVENT_TYPE access="READ_ONLY">Lecture</EVENT_TYPE>
+      <DTM_EVENT access="READ_ONLY">May</DTM_EVENT>
+      <DTD_EVENT access="READ_ONLY">25</DTD_EVENT>
+      <DTY_EVENT access="READ_ONLY">77</DTY_EVENT>
+      <CAL_TEACH_HRS access="READ_ONLY">2</CAL_TEACH_HRS>
+    </INSTRUCT_TAUGHT>
+  </Record>
+  <Record PennStateHealthUsername="hgranger">
+    <INSTRUCT_TAUGHT>
+      <COURSE_YEAR access="READ_ONLY">1997-1998</COURSE_YEAR>
+      <COURSE_TITLE access="READ_ONLY">Potions</COURSE_TITLE>
+      <EVENT_TITLE access="READ_ONLY">FTF REQ Various Rooms 10-12 PBL - EndoRepro PBL 1402 - Thyroid</EVENT_TITLE>
+      <EVENT_TYPE access="READ_ONLY">Sm Grp Facilitation</EVENT_TYPE>
+      <DTM_EVENT access="READ_ONLY">June</DTM_EVENT>
+      <DTD_EVENT access="READ_ONLY">26</DTD_EVENT>
+      <DTY_EVENT access="READ_ONLY">97</DTY_EVENT>
+      <CAL_TEACH_HRS access="READ_ONLY">7</CAL_TEACH_HRS>
+    </INSTRUCT_TAUGHT>
+  </Record>
+</Data>
+'
+      )
+      end
+    end
+end
