1f714c56e5333f4c6212be254ab9564941ef21fb
ddecollo <dad5925@psu.edu>
2023-12-15 13:41:52 -0500

    event_date years are input as 4 digits not 2

--- spec/importers/com_data/com_effort_xml_builder_spec.rb
+++ spec/importers/com_data/com_effort_xml_builder_spec.rb
@@ -8,15 +8,15 @@ RSpec.describe ComData::ComEffortXmlBuilder do
        'COURSE_YEAR' => '1976-1977',
        'EVENT_TYPE' => 'Lecture',
        'EVENT' => 'FTF REQ Various Rooms 10-12 PBL - EndoRepro PBL 1402 - Thyroid',
-       'EVENT_DATE' => '5/25/77',
+       'EVENT_DATE' => '5/25/1977',
        'UME_CALCULATED_TEACHING_WHILE_NON_BILLING_EFFORT__HRS_' => 2.5 },
      { 'FACULTY_USERNAME' => 'hgranger',
        'FACULTY_NAME' => 'Granger Hermione',
        'COURSE' => 'Potions',
-       'COURSE_YEAR' => '1997-1998',
+       'COURSE_YEAR' => '2022-2023',
        'EVENT_TYPE' => 'Sm Grp Facilitation',
        'EVENT' => 'FTF REQ Various Rooms 10-12 PBL - EndoRepro PBL 1402 - Thyroid',
-       'EVENT_DATE' => '6/26/97',
+       'EVENT_DATE' => '6/26/2023',
        'UME_CALCULATED_TEACHING_WHILE_NON_BILLING_EFFORT__HRS_' => 7.25 },
      { 'FACULTY_USERNAME' => 'notCOM',
        'FACULTY_NAME' => 'Not MD',
@@ -24,7 +24,7 @@ RSpec.describe ComData::ComEffortXmlBuilder do
        'COURSE_YEAR' => '2022-2023',
        'EVENT_TYPE' => 'Lacture',
        'EVENT' => 'Test Event for employee not in College of Medicine',
-       'EVENT_DATE' => '1/1/23',
+       'EVENT_DATE' => '1/1/2023',
        'UME_CALCULATED_TEACHING_WHILE_NON_BILLING_EFFORT__HRS_' => 1 }]
   end
 
@@ -66,13 +66,13 @@ RSpec.describe ComData::ComEffortXmlBuilder do
   </Record>
   <Record PennStateHealthUsername="hgranger">
     <INSTRUCT_TAUGHT>
-      <COURSE_YEAR access="READ_ONLY">1997-1998</COURSE_YEAR>
+      <COURSE_YEAR access="READ_ONLY">2022-2023</COURSE_YEAR>
       <COURSE_TITLE access="READ_ONLY">Potions</COURSE_TITLE>
       <EVENT_TITLE access="READ_ONLY">FTF REQ Various Rooms 10-12 PBL - EndoRepro PBL 1402 - Thyroid</EVENT_TITLE>
       <EVENT_TYPE access="READ_ONLY">Sm Grp Facilitation</EVENT_TYPE>
       <DTM_EVENT access="READ_ONLY">June</DTM_EVENT>
       <DTD_EVENT access="READ_ONLY">26</DTD_EVENT>
-      <DTY_EVENT access="READ_ONLY">1997</DTY_EVENT>
+      <DTY_EVENT access="READ_ONLY">2023</DTY_EVENT>
       <CAL_TEACH_HRS access="READ_ONLY">7.25</CAL_TEACH_HRS>
     </INSTRUCT_TAUGHT>
   </Record>
