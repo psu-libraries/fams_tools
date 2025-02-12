6f976a7f25a8d146e9f94ae44527734f492d86c4
Ben Browner <bnbrowner@gmail.com>
2025-02-12 12:29:10 -0500

    rspec silliness over

--- spec/services/ai_disable_client_spec.rb
+++ spec/services/ai_disable_client_spec.rb
@@ -1,28 +1,70 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe AiDisableClient, type: :service do
-  let(:client) { AiDisableClient.new }
+RSpec.describe AiDisableClient do
+  let(:username) { 'test_username' }
+  let(:password) { 'test_password' }
+  let(:client) { described_class.new(username, password) }
   let(:uid) { 'user1' }
 
-  before do
-    stub_request(:get, /betawebservices.digitalmeasures.com/)
-      .to_return(status: 200, body: "<User><Username>#{uid}</Username></User>", headers: { 'Content-Type' => 'text/xml' })
-
-    stub_request(:put, /betawebservices.digitalmeasures.com/)
-      .to_return(status: 200, body: "", headers: { 'Content-Type' => 'text/xml' })
-  end
-
   describe '#user' do
-    it 'fetches user data' do
-      response = client.user(uid)
-      expect(response.body).to include("<Username>#{uid}</Username>")
+    let(:response_body) do
+      <<-XML
+<?xml version="1.0" encoding="UTF-8"?>
+<User>
+  <FirstName>redacted</FirstName>
+</User>
+      XML
+    end
+
+    context 'when user is found' do
+      let(:success_response) do
+        instance_double(
+          HTTParty::Response,
+          code: 200,
+          body: response_body
+        )
+      end
+
+      it 'fetches user data' do
+        allow(described_class).to receive(:get)
+          .with("/User/USERNAME:#{uid}", hash_including(basic_auth: { username:, password: }))
+          .and_return(success_response)
+
+        result = client.user(uid)
+        expect(result.body).to include("<FirstName>redacted</FirstName>")
+      end
     end
   end
 
   describe '#enable_user' do
-    it 'sends a request to enable or disable a user' do
-      response = client.enable_user(uid, false)
-      expect(response.code).to eq(200)
+    context 'when enabling or disabling a user' do
+      let(:success_response) do
+        instance_double(
+          HTTParty::Response,
+          code: 200,
+          body: ''
+        )
+      end
+
+      it 'sends a request to enable a user' do
+        allow(described_class).to receive(:put)
+          .with("/User/USERNAME:#{uid}", hash_including(basic_auth: { username:, password: }, body: "<User enabled=\"true\"/>"))
+          .and_return(success_response)
+
+        result = client.enable_user(uid, true)
+        expect(result.code).to eq(200)
+      end
+
+      it 'sends a request to disable a user' do
+        allow(described_class).to receive(:put)
+          .with("/User/USERNAME:#{uid}", hash_including(basic_auth: { username:, password: }, body: "<User enabled=\"false\"/>"))
+          .and_return(success_response)
+
+        result = client.enable_user(uid, false)
+        expect(result.code).to eq(200)
+      end
     end
   end
 end
