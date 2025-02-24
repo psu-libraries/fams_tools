353977e9cd65b744608bc65da13425888ae93e3f
Ben Browner <bnbrowner@gmail.com>
2025-02-24 11:21:26 -0500

    new rspec

--- spec/mailers/error_mailer_spec.rb
+++ spec/mailers/error_mailer_spec.rb
@@ -4,32 +4,55 @@ RSpec.describe ErrorMailer, type: :mailer do
   describe '#error_email' do
     let(:integration_name) { 'TestIntegration' }
     let(:log_path) { Rails.root.join('spec/fixtures/files/test_error.log') }
-    let(:mail) { described_class.error_email(integration_name, log_path) }
     let(:current_time) { DateTime.now.strftime('%B %d, %Y at %I:%M %p') }
 
-    before do
-      FileUtils.mkdir_p(File.dirname(log_path))
-      File.write(log_path, 'Test error log content')
+    context 'when log file exists' do
+      let(:mail) { described_class.error_email(integration_name, log_path) }
+
+      before do
+        FileUtils.mkdir_p(File.dirname(log_path))
+        File.write(log_path, 'Test error log content')
+      end
+
+      after do
+        FileUtils.rm_f(log_path)
+      end
+
+      it 'renders the headers' do
+        expect(mail.subject).to eq("AI Integration #{integration_name} Error")
+        expect(mail.from).to eq(['L-FAMS@LISTS.PSU.EDU'])
+      end
+
+      it 'renders the body' do
+        expect(mail.body.encoded).to include(integration_name)
+        expect(mail.body.encoded).to include(current_time)
+        expect(mail.body.encoded).to include('The full error log has been attached')
+      end
+
+      it 'includes the error log as an attachment' do
+        expect(mail.attachments.count).to eq(1)
+        expect(mail.attachments['error.log'].body.raw_source).to eq('Test error log content')
+      end
     end
 
-    after do
-      FileUtils.rm_f(log_path)
-    end
+    context 'when log file does not exist' do
+      let(:nonexistent_log_path) { Rails.root.join('spec/fixtures/files/nonexistent.log') }
+      let(:mail) { described_class.error_email(integration_name, nonexistent_log_path) }
 
-    it 'renders the headers' do
-      expect(mail.subject).to eq("AI Integration #{integration_name} Error")
-      expect(mail.from).to eq(['L-FAMS@LISTS.PSU.EDU'])
-    end
+      it 'renders the headers' do
+        expect(mail.subject).to eq("AI Integration #{integration_name} Error")
+        expect(mail.from).to eq(['L-FAMS@LISTS.PSU.EDU'])
+      end
 
-    it 'renders the body' do
-      expect(mail.body.encoded).to include(integration_name)
-      expect(mail.body.encoded).to include(current_time)
-      expect(mail.body.encoded).to include('The full error log has been attached')
-    end
+      it 'renders the body' do
+        expect(mail.body.encoded).to include(integration_name)
+        expect(mail.body.encoded).to include(current_time)
+        expect(mail.body.encoded).to include('No error log was available')
+      end
 
-    it 'includes the error log as an attachment' do
-      expect(mail.attachments.count).to eq(1)
-      expect(mail.attachments['error.log'].body.raw_source).to eq('Test error log content')
+      it 'does not include any attachments' do
+        expect(mail.attachments.count).to eq(0)
+      end
     end
   end
 end
