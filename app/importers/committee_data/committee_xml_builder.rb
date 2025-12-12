2dadff243976bc2bfbc8ddb12d20c8337180b2e3
usmannsiddiqui <usmansiddiquiii645@gmail.com>
2025-12-12 09:59:54 -0500

    Fix RuboCop trailing whitespace in committee XML builder

--- app/importers/committee_data/committee_xml_builder.rb
+++ app/importers/committee_data/committee_xml_builder.rb
@@ -6,7 +6,7 @@ class CommitteeData::CommitteeXmlBuilder
       Faculty.includes(:committees)
              .joins(:committees)
              .distinct
-             .find_in_batches(batch_size: 8) do |batch|
+             .find_in_batches(batch_size: 20) do |batch|
         i << build_xml(batch)
       end
     end
@@ -24,18 +24,17 @@ class CommitteeData::CommitteeXmlBuilder
           xml.Record(username: faculty.access_id) do
             committees.each do |committee|
               xml.DSL do
-                
-                xml.ROLE       committee.role
-                xml.ROLE_OTHER ''              # TODO: add column later if needed
-                xml.TYPE       ''              # e.g. "Other"
-                xml.TYPE_OTHER ''              # e.g. "Team Support"
-                xml.COMPSTAGE  ''              # e.g. "Completed"
+                xml.ROLE committee.role
+                xml.ROLE_OTHER '' # TODO: add column later if needed
+                xml.TYPE '' # e.g. "Other"
+                xml.TYPE_OTHER '' # e.g. "Team Support"
+                xml.COMPSTAGE '' # e.g. "Completed"
 
                 # Student info block – one per committee for now
                 xml.DSL_STUDENT do
                   xml.FNAME committee.student_fname
                   xml.LNAME committee.student_lname
-                  xml.DEG   committee.degree_type
+                  xml.DEG committee.degree_type
                   xml.TITLE committee.thesis_title
                   xml.MAJOR ''
                   xml.DTM_GRAD ''
@@ -45,16 +44,16 @@ class CommitteeData::CommitteeXmlBuilder
 
                 xml.COURSEPRE ''
                 xml.COURSENUM ''
-                xml.CHOURS    ''
+                xml.CHOURS ''
                 xml.DTM_START ''
                 xml.DTD_START ''
                 xml.DTY_START ''
-                xml.DTM_END   ''
-                xml.DTD_END   ''
-                xml.DTY_END   ''
+                xml.DTM_END ''
+                xml.DTD_END ''
+                xml.DTY_END ''
 
                 xml.ABSTRACT ''
-                xml.DETAILS  ''
+                xml.DETAILS ''
               end
             end
           end
