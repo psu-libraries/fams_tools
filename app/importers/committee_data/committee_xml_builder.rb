b110dcdbc8426d3860485b344e65244480492235
usmannsiddiqui <usmansiddiquiii645@gmail.com>
2025-12-12 11:45:38 -0500

    Trim committee XML export to minimal DSL fields validated in Postman

--- app/importers/committee_data/committee_xml_builder.rb
+++ app/importers/committee_data/committee_xml_builder.rb
@@ -25,42 +25,19 @@ class CommitteeData::CommitteeXmlBuilder
             committees.each do |committee|
               xml.DSL do
                 xml.ROLE committee.role
-                xml.ROLE_OTHER '' # TODO: add column later if needed
-                xml.TYPE '' # e.g. "Other"
-                xml.TYPE_OTHER '' # e.g. "Team Support"
-                xml.COMPSTAGE '' # e.g. "Completed"
 
-                # Student info block – one per committee for now
                 xml.DSL_STUDENT do
                   xml.FNAME committee.student_fname
                   xml.LNAME committee.student_lname
                   xml.DEG committee.degree_type
                   xml.TITLE committee.thesis_title
-                  xml.MAJOR ''
-                  xml.DTM_GRAD ''
-                  xml.DTD_GRAD ''
-                  xml.DTY_GRAD ''
-                end
-
-                xml.COURSEPRE ''
-                xml.COURSENUM ''
-                xml.CHOURS ''
-                xml.DTM_START ''
-                xml.DTD_START ''
-                xml.DTY_START ''
-                xml.DTM_END ''
-                xml.DTD_END ''
-                xml.DTY_END ''
-
-                xml.ABSTRACT ''
-                xml.DETAILS ''
               end
             end
           end
         end
       end
     end
-
+  end 
     builder.to_xml
   end
 end
