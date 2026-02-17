15f502a73da0994c4756d744599f12660ce68b65
usmannsiddiqui <usmansiddiquiii645@gmail.com>
2026-02-17 12:47:38 -0500

    Skeleton initialized, didnt save file initially

---
+++ app/clients/etda/committee_records_client.rb
@@ -0,0 +1,12 @@
+module ETDA
+  class ComitteeRecordsClient
+    def initialize
+      @base_url = ENV['ETDA_API_URL']
+      @token = ENV['ETDA_API_TOKEN']
+    end
+
+    def faculty_committees (access_id)
+      
+    end
+  end
+end
