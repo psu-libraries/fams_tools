74c5441881fc7fe438fd12640d21da98908bfedd
usmannsiddiqui <usmansiddiquiii645@gmail.com>
2026-03-19 11:52:49 -0400

    fixed up regex in committe normalizer and updated envrc.example

--- app/services/committee_role_normalizer.rb
+++ app/services/committee_role_normalizer.rb
@@ -1,14 +1,15 @@
 class CommitteeRoleNormalizer
   PRIORITY_REGEX = [
-    ['Co-Chairperson', %r{\b(co[-\s]?chair|co[-\s]?chairperson|committee chair/co-chair)\b}i],
-    ['Chairperson', /\b(chairperson|chair of committee|committee chair|chair)\b/i],
-    ['Co-Advisor', /\b(co[-\s]?dissertation\s*advis(or|er)|co[-\s]?advisor)\b/i],
-    ['Advisor', /\b(dissertation\s*advis(?:o?r|er)|advis(?:o?r|er))\b/i],
-    ['Supervisor', /\bsupervisor\b/i],
-    ['Mentor', /\bmentor\b/i],
-    ['Second Reader', /\bsecond\s+reader\b/i],
-    ['Reader', /\breader\b/i],
-    ['Member', /\b(member|rep|represent|representative|substitute)\b/i]
+    ['Co-Chairperson', %r{(co[-\s]?chair|co[-\s]?chairperson|committee chair/co-chair)}i],
+    ['Chairperson', /(chairperson|chair of committee|committee chair|chair.)/i],
+    ['Co-Advisor', /(co[-\s]?dissertation\s*advis(or|er)|co[-\s]?advisor)/i],
+    ['Advisor', /(dissertation\s*advis(?:o?r|er)|advis(?:o?r|er))/i],
+    ['Supervisor', /supervisor/i],
+    ['Mentor', /mentor/i],
+    ['Second Reader', /second\s+reader/i],
+    ['Reader', /reader/i],
+    ['Member', /(member|rep|represent|representative|substitute)/i]
+    
   ].freeze
 
   def self.normalize(raw_name)
