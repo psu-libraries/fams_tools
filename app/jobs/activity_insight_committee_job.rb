16d78c64ef44faec0a55c122103c763aa17601da
Joao Vitor Barros da Silva <jvitorbarros15@gmail.com>
2026-06-18 14:36:23 -0400

    Replace since date arg with month_range integer in committee job

--- app/jobs/activity_insight_committee_job.rb
+++ app/jobs/activity_insight_committee_job.rb
@@ -1,20 +1,17 @@
 class ActivityInsightCommitteeJob < ApplicationJob
-  def integrate(target, since: nil)
-    window_since, window_until = since ? [since, since.next_month] : default_window
-    CommitteeData::EtdaImporter.new.import_all(since: window_since, until_date: window_until)
+  def integrate(target, month_range: 1)
+    since_date  = month_range.months.ago.to_date.change(day: 10)
+    until_date  = Date.current.change(day: 10)
+    CommitteeData::EtdaImporter.new.import_all(since: since_date, until_date:)
 
-    builder   = CommitteeData::CommitteeXmlBuilder.new
-    xml_enum  = builder.xmls_enumerator
+    builder    = CommitteeData::CommitteeXmlBuilder.new
+    xml_enum   = builder.xmls_enumerator
 
     integrator = ActivityInsight::IntegrateData.new(xml_enum, target, :post)
     integrator.integrate
   end
 
   private
-
-  def default_window
-    [Date.current.prev_month.change(day: 10), Date.current.change(day: 10)]
-  end
 
   def name
     'Committee Integration'
